9c65a72f5251f7fa49eb5ba25fa1b36115ad038d
ukwuani <Chimbudike1>
2020-04-11 08:59:37 +0100

    input data change

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    coon
+    return {
+        data,
+        impact: calculateImpact(10, inputData),
+        severeImpact: calculateImpact(50, inputData),
+    }
+};
 
 export default covid19ImpactEstimator;
+
+const inputData = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 27,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+};
