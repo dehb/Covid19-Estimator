f134a577c4c567d3a0568190a3599ee26ae7a3b3
ukwuani <Chimbudike1>
2020-04-11 09:21:13 +0100

    input data

--- src/estimator.js
+++ src/estimator.js
@@ -1,22 +1,22 @@
 
-const calculateImpact = (number, data) => {
-    let currentlyInfected = data.reportedCases * number;
-    let infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor((data.timeToElapse / 3)));
-    let severeCasesByRequestedTime = Math.floor((15/100) * infectionsByRequestedTime);
-    let hospitalBedsByRequestedTime = severeCasesByRequestedTime - Math.floor((35/100) * data.totalHospitalBeds);
-    let casesForICUByRequestedTime = Math.floor((5/100) * infectionsByRequestedTime);
-    let casesForVentilatorsByRequestedTime =Math.floor( (2/100) * infectionsByRequestedTime);
-    let dollarsInFlight = infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse;
-    return {
-        currentlyInfected,
-        infectionsByRequestedTime,
-        severeCasesByRequestedTime,
-        hospitalBedsByRequestedTime,
-        casesForICUByRequestedTime,
-        casesForVentilatorsByRequestedTime,
-        dollarsInFlight
-    }
-};
+// const calculateImpact = (number, data) => {
+//     let currentlyInfected = data.reportedCases * number;
+//     let infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor((data.timeToElapse / 3)));
+//     let severeCasesByRequestedTime = Math.floor((15/100) * infectionsByRequestedTime);
+//     let hospitalBedsByRequestedTime = severeCasesByRequestedTime - Math.floor((35/100) * data.totalHospitalBeds);
+//     let casesForICUByRequestedTime = Math.floor((5/100) * infectionsByRequestedTime);
+//     let casesForVentilatorsByRequestedTime =Math.floor( (2/100) * infectionsByRequestedTime);
+//     let dollarsInFlight = infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+//     return {
+//         currentlyInfected,
+//         infectionsByRequestedTime,
+//         severeCasesByRequestedTime,
+//         hospitalBedsByRequestedTime,
+//         casesForICUByRequestedTime,
+//         casesForVentilatorsByRequestedTime,
+//         dollarsInFlight
+//     }
+// };
 
 const covid19ImpactEstimator = (data) => {
     return {
