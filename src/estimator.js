10e85e569764b9f5323dfac9b5dbf1a24591e2a7
ukwuani <Chimbudike1>
2020-04-12 19:38:59 +0100

    comit

--- src/estimator.js
+++ src/estimator.js
@@ -2,17 +2,25 @@ const calculateImpact = (number, data) => {
   const hospitalBed = data.totalHospitalBeds;
   const incomPop = data.region.avgDailyIncomePopulation;
   const incomUSD = data.region.avgDailyIncomeInUSD;
-  const timElapse = data.timeToElapse;
+ 
+  if (data.periodType === 'weeks'){
+    data.timeToElapse *= 7;
+  }else if (data.periodType === 'months'){
+    data.timeToElapse *= 30;
+  }
+  const days = data.timeToElapse;
+  const factor = Math.trunc(days / 3);
+
   //  CHALLENGE 1
   const currentlyInfected = data.reportedCases * number;
-  const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor((data.timeToElapse / 3)));
+  const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor((days / 3)));
   //  CHALLENGE 2
   const severeCasesByRequestedTime = Math.floor((15 / 100) * infectionsByRequestedTime);
   const hospitalBedsByRequestedTime = severeCasesByRequestedTime - Math.floor(0.35 * hospitalBed);
   //  CHALLENGE 3
   const casesForICUByRequestedTime = Math.floor((5 / 100) * infectionsByRequestedTime);
   const casesForVentilatorsByRequestedTime = Math.floor((2 / 100) * infectionsByRequestedTime);
-  const dollarsInFlight = Math.floor(infectionsByRequestedTime * incomPop * incomUSD * timElapse);
+  const dollarsInFlight = Math.floor(infectionsByRequestedTime * incomPop * incomUSD * days);
   return {
     //  CHALLENGE 1
     currentlyInfected,
@@ -26,20 +34,6 @@ const calculateImpact = (number, data) => {
     dollarsInFlight
   };
 };
-
-// const inputData = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
 
 const covid19ImpactEstimator = (data) => ({
   data,
