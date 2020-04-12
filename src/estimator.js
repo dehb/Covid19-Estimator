fe0b07b308c6e333bf2a554db34fa19d74102ef7
ukwuani <Chimbudike1>
2020-04-12 20:09:56 +0100

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -8,7 +8,6 @@ const calculateImpact = (number, data) => {
     data.timeToElapse *= 30;
   }
   const days = data.timeToElapse;
-  //const factor = Math.trunc(days / 3);
   //  CHALLENGE 1
   const currentlyInfected = data.reportedCases * number;
   const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor((days / 3)));
